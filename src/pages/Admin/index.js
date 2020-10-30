8c493aaf5586683c6c7ca64680e2e448b83fba0c
lailikumala92 <lailikumala92@gmail.com>
2020-10-30 13:56:26 +0700

    change import file Admin/index.js from Main to Topup & AddTopup

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -3,7 +3,7 @@ import Users from './Users'
 import EditUser from './EditUser'
 import DetailUser from './Main'
 import AddUser from './Main'
-import Topup from './Main'
-import AddTopup from './Main'
+import Topup from './Topup'
+import AddTopup from './AddTopup'
 
 export { Main, Users, EditUser, DetailUser, AddUser, Topup, AddTopup };
